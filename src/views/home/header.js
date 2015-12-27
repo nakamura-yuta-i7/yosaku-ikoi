9727225a549fa3c677b6b18778f795cdfb71db84
Yuta Nakamura <yuta.nakamura.i7@gmail.com>
2015-12-27 05:08:36 +0000

    不要ファイル削除

--- src/views/home/header.js
+++
@@ -1,8 +0,0 @@
-require("./header.scss")
-let $header = $(`
-	<header>
-		<h1>憩いの掲示板</h1>
-	</header>
-`)
-
-module.exports = $header
--- src/views/home/home.php
+++
@@ -1,4 +0,0 @@
-<?php include dirname(__FILE__) . "/../common/header.php"; ?>
-<link rel="stylesheet" href="/dist/home.css">
-<script src="/dist/home.js"></script>
-<?php include dirname(__FILE__) . "/../common/footer.php"; ?>
